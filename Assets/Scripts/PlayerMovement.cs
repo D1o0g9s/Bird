04bde955efc45b66e5682faafac8330c8be4ebef
unknown <origamijr@sbcglobal.net>
2018-02-24 05:01:24 -0800

    got timings

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    private long lowerTick = 0;
+    private long upperTick = 0;
+
+    GameObject lowerCollider;
+    GameObject upperCollider;
+
+    TriggerTime lowerColliderScript;
+    TriggerTime upperColliderScript;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +20,8 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        upperTick = upperColliderScript.collisionTime;
+
+        if (lowerTick !=lowerColliderScript.collisionTime)
 	}
 }
---
+++ Assets/Scripts/TriggerTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTime : MonoBehaviour {
+
+    public long collisionTime;
+
+    private void OnTriggerEnter(Collider other) {
+        collisionTime = DateTime.Now.Ticks;
+        Debug.Log(collisionTime);
+    }
+}
