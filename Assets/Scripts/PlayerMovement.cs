70125dec4953d5aff5edea2a3e8f1dddeb042b89
unknown <origamijr@sbcglobal.net>
2018-02-24 11:31:19 -0800

    city thing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-    public const float maxXVelocity = 10.0f;
+    //public const float maxXVelocity = 10.0f;
     public const float XVelocityScalingFactor = 0.2f; // scales deltaY of controller from ground / lower collider
 
     private long lLowerTick = 0;
@@ -13,6 +13,7 @@ public class PlayerMovement : MonoBehaviour {
     private long rLowerTick = 0;
     private long rUpperTick = 0;
 
+    [SerializeField] private GameObject camera;
     [SerializeField] private GameObject lowerCollider;
     [SerializeField] private GameObject upperCollider;
     [SerializeField] private GameObject leftControllerCollider;
@@ -50,6 +51,19 @@ public class PlayerMovement : MonoBehaviour {
             gameObject.GetComponent<Rigidbody>().AddForce(0.0f, Mathf.Max(0.0f, Mathf.Min(0.3f / (float) (rLowerTick - rUpperTick) * (float) TimeSpan.TicksPerSecond, 10.0f)), 0.0f, ForceMode.VelocityChange);
         }
 
-        
+        float deltaLeftY = leftControllerCollider.transform.position.y - lowerCollider.transform.position.y - 1;
+        float deltaRightY = rightControllerCollider.transform.position.y - lowerCollider.transform.position.y - 1;
+
+        if (deltaLeftY < 0 && deltaRightY > 0 /*&& Mathf.Abs(deltaLeftY) > 0.5 && Mathf.Abs(deltaRightY) > 0.5*/) {
+            Debug.Log("turning right");
+            Vector3 forwardDir = camera.transform.forward;
+            Vector3 upwardDir = new Vector3(0.0f, 1.0f, 0.0f);
+            this.GetComponent<Rigidbody>().velocity += Vector3.Cross(forwardDir, upwardDir).normalized * XVelocityScalingFactor;
+        } else if (deltaLeftY > 0 && deltaRightY < 0 /*&& Mathf.Abs(deltaLeftY) > 0.5 && Mathf.Abs(deltaRightY) > 0.5*/) {
+            Debug.Log("turning left");
+            Vector3 forwardDir = camera.transform.forward;
+            Vector3 upwardDir = new Vector3(0.0f, 1.0f, 0.0f);
+            this.GetComponent<Rigidbody>().velocity -= Vector3.Cross(forwardDir, upwardDir).normalized * XVelocityScalingFactor;
+        }
     }
 }
